06115d5801987d2b4a8cf41217ba37e1bf778fa6
Diyora <diyoraabdurakhmonova@gmail.com>
2023-05-09 23:19:43 -0400

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+  excludeSpecPattern: [
+      'cypress/e2e/1-getting-started/*',
+      'cypress/e2e/1-advanced-examples/*',
+  ]
+    },
+  },
+});
